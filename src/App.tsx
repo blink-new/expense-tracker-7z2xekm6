b31a4938c9630ca27cd85ffbf21d74b287432db3
ShadowWalker2014 <kai.jiabo.feng@gmail.com>
2025-03-25 23:18:10 +0000

    Add Missing UI Components

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,20 @@
 
-import { BrowserRouter as Router } from 'react-router-dom';
-import { ClerkProvider } from '@clerk/clerk-react';
-import { AppRoutes } from './routes';
-import { Toaster } from './components/ui/toaster';
+import { ClerkProvider } from "@clerk/clerk-react"
+import { BrowserRouter as Router } from "react-router-dom"
+import { AppRoutes } from "./routes"
+import { Toaster } from "./components/ui/toaster"
 
-const clerkPubKey = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY;
+const CLERK_PUBLISHABLE_KEY = "pk_test_Y3JlZGlibGUtZWZ0LTU0LmNsZXJrLmFjY291bnRzLmRldiQ"
 
 function App() {
   return (
-    <ClerkProvider publishableKey={clerkPubKey}>
+    <ClerkProvider publishableKey={CLERK_PUBLISHABLE_KEY}>
       <Router>
         <AppRoutes />
         <Toaster />
       </Router>
     </ClerkProvider>
-  );
+  )
 }
 
-export default App;
+export default App
