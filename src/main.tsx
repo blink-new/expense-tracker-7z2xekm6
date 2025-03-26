6b117700bfa5c9f34f31a46958167ad0ff6e6d38
ShadowWalker2014 <kai.jiabo.feng@gmail.com>
2025-03-26 03:23:48 +0000

    Remove duplicate ClerkProvider from main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -2,17 +2,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { Toaster } from 'react-hot-toast'
-import { ClerkProvider } from '@clerk/clerk-react'
 import App from './App'
 import './index.css'
 
-const clerkPubKey = 'pk_test_Y3JlZGlibGUtZWZ0LTU0LmNsZXJrLmFjY291bnRzLmRldiQ'
-
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <ClerkProvider publishableKey={clerkPubKey}>
-      <Toaster position="top-right" />
-      <App />
-    </ClerkProvider>
+    <Toaster position="top-right" />
+    <App />
   </React.StrictMode>,
 )
