3fa402eb5eb93050528696fc6f5589f6bc5af55d
ShadowWalker2014 <kai.jiabo.feng@gmail.com>
2025-03-26 03:23:18 +0000

    Add Clerk publishable key

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,18 @@
+
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { Toaster } from 'react-hot-toast'
+import { ClerkProvider } from '@clerk/clerk-react'
 import App from './App'
 import './index.css'
 
+const clerkPubKey = 'pk_test_Y3JlZGlibGUtZWZ0LTU0LmNsZXJrLmFjY291bnRzLmRldiQ'
+
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <Toaster position="top-right" />
-    <App />
+    <ClerkProvider publishableKey={clerkPubKey}>
+      <Toaster position="top-right" />
+      <App />
+    </ClerkProvider>
   </React.StrictMode>,
-) 
+)
